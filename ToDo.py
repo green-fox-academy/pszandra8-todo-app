24fb809eb136075073f68f531ae0ea7a910ae71e
Alexandra Pal <pszandra8@gmail.com>
2017-12-15 11:10:07 +0100

    Classes has been added, some printing has been given

--- ToDo.py
+++ ToDo.py
@@ -1,80 +1,87 @@
 import sys
 filename = "Text.txt"
 
-def print_usage_info():
-    print(
-        "Command Line Todo application\n"
-        "=============================\n"
-        "\nCommand line arguments:\n"
-        "-l   Lists all the tasks\n" 
-        "-a   Adds a new task\n"
-        "-r   Removes a task\n"
-        "-c   Completes a task\n")
+class ToDo(object):
+    def print_usage_info(self):
+        print(
+            "Command Line Todo application\n"
+            "=============================\n"
+            "\nCommand line arguments:\n"
+            "-l   Lists all the tasks\n" 
+            "-a   Adds a new task\n"
+            "-r   Removes a task\n"
+            "-c   Completes a task\n")
 
-def open_Text():
-    try:
-        t_file = open(filename, "r")
-        lines = t_file.readlines()
-        t_file.close()
-        counter = 0
-        if len(lines) == 0:
-            print("There are no todos for today")
-        else:
-            for i in range(len(lines)):
-                counter += 1
-                print(str(counter) + " - " + lines[i].strip())
-    except FileNotFoundError:
-        return 0
+    def open_Text(self):
+        try:
+            t_file = open(filename, "r")
+            lines = t_file.readlines()
+            t_file.close()
+            counter = 0
+            if len(lines) == 0:
+                print("There are no todos for today")
+                print_usage_info()
+            else:
+                for i in range(len(lines)):
+                    counter += 1
+                    print(str(counter) + " - " + lines[i].strip())
+        except FileNotFoundError:
+            print("Dod not find the file")
 
-def add_to_Text():
-    try:
-        t_file = open(filename, "a")
-        user_input = sys.argv[2]
-        t_file.write("\n[ ] " + user_input)
-        t_file.close()
-    except IOError:
-        print("Unable to write file.")
+    def add_to_Text(self):
+        try:
+            t_file = open(filename, "a")
+            user_input = sys.argv[2]
+            t_file.write("\n[ ] " + user_input)
+            t_file.close()
+            print("Task has been added to your ToDos")
+        except IOError:
+            print("Unable to write file.")
 
-def remove_Text():
-    try:
-        t_file = open(filename, "r")
-        lines = t_file.readlines()
-        lines_for_write = open(filename, "w")
-        if sys.argv[2] == 1:
-            lines.pop(0)
-        else:
+    def remove_Text(self):
+        try:
+            t_file = open(filename, "r")
+            lines = t_file.readlines()
+            lines_for_write = open(filename, "w")
+            if sys.argv[2] == 1:
+                lines.pop(0)
+                print("Task has been removed from your ToDos")
+            else:
+                index = int(sys.argv[2]) - 1
+                lines.pop(index)
+                for line in lines:
+                    lines_for_write.write(line)
+                print("Task has been removed from your ToDos")
+            t_file.close()
+        except IOError:
+            print("Unable to write file.")
+
+    def check(self):
+        try:
+            t_file = open(filename, "r")
+            lines = t_file.readlines()
+            t_file.close()
+            lines_for_write = open(filename, "w")
             index = int(sys.argv[2]) - 1
-            lines.pop(index)
-            for line in lines:
-                lines_for_write.write(line)
-        t_file.close()
-    except IOError:
-        print("Unable to write file.")
+            lines[index] = "[x]" + lines[index][3:]
+            lines_for_write.writelines(lines)
+            t_file.close()
+            print("The task has been checked")
+        except FileNotFoundError:
+            print("Dod not find the file")
 
-def check():
-    try:
-        t_file = open(filename, "r")
-        lines = t_file.readlines()
-        t_file.close()
-        lines_for_write = open(filename, "w")
-        index = int(sys.argv[2]) - 1
-        lines[index] = "[x]" + lines[index][3:]
-        lines_for_write.writelines(lines)
-        t_file.close()
-            
-    except FileNotFoundError:
-        return 0
-    
+todo = ToDo()
+
 if len(sys.argv) == 1:
-    print_usage_info()
+    todo.print_usage_info()
 elif len(sys.argv) == 2 or len(sys.argv) == 3:
     if sys.argv[1] == "-l":
-        open_Text()
+        todo.open_Text()
     elif sys.argv[1] == "-a":
-        add_to_Text()
+        todo.add_to_Text()
     elif sys.argv[1] == "-r":
-        remove_Text()
+        todo.remove_Text()
     elif sys.argv[1] == "-c":
-        check()
+        todo.check()
     else:
         print("Unsupported argument")
